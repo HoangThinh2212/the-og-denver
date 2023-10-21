033c02cf1c620eac205405646e9d433dd9f7f063
daveli1703 <lychihung2004@gmail.com>
2023-10-21 23:27:58 +0700

    update

---
+++ js/happenings.js
@@ -0,0 +1,9 @@
+function redirect1() {
+    window.location.href = "https://www.theogdenver.com/event/halloween-boo-runch/"; 
+}
+function redirect2() {
+    window.location.href = "https://www.theogdenver.com/event/brunching-for-change/"; 
+}
+function redirect3() {
+    window.location.href = "https://www.theogdenver.com/event/eat-drink-support-local/"; 
+}
