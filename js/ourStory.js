1a1254ef3d62fd17b30a1a44f2bc42c802d2291a
daveli1703 <lychihung2004@gmail.com>
2023-10-25 11:53:25 +0700

    <update>

--- js/ourStory.js
+++ js/ourStory.js
@@ -1,28 +1,47 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overLay = document.getElementById('overLay')
-openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = document.querySelector(button.dataset.modalTarget)
-        openModal(modal)
-    })
-})
-closeModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = button.closest('.modal')
-        closeModal(modal)
-    })
-})
-function openModal(modal) {
-    if (modal == null) return
-    modal.classList.add('active')
-    overlay.classList.add('active')
+// const openModalButtons = document.querySelectorAll('[data-modal-target]')
+// const closeModalButtons = document.querySelectorAll('[data-close-button]')
+// const overLay = document.getElementById('overLay')
+// openModalButtons.forEach(button => {
+//     button.addEventListener('click', () => {
+//         const modal = document.querySelector(button.dataset.modalTarget)
+//         openModal(modal)
+//     })
+// })
+// closeModalButtons.forEach(button => {
+//     button.addEventListener('click', () => {
+//         const modal = button.closest('.modal')
+//         closeModal(modal)
+//     })
+// })
+// function openModal(modal) {
+//     if (modal == null) return
+//     modal.classList.add('active')
+//     overlay.classList.add('active')
 
-}
-function closeModal(modal) {
-    if (modal == null) return
-    modal.classList.remove('active')
-    overlay.classList.remove('active')
+// }
+// function closeModal(modal) {
+//     if (modal == null) return
+//     modal.classList.remove('active')
+//     overlay.classList.remove('active')
 
+// }
+const openModalBtn = document.getElementById("openModalBtn");
+const closeBtn = document.getElementById("close-btn");
+const overlay = document.getElementById("overLay");
+
+// Function to open or close the modal
+function toggleModal() {
+  const personalInfo = document.querySelector(".personalinfo");
+  personalInfo.classList.toggle("active");
+  overlay.classList.toggle("active");
 }
+openModalBtn.addEventListener("click", toggleModal);
+
+closeBtn.addEventListener("click", toggleModal);
+
+
+
+
+
+
 
